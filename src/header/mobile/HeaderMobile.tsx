8004509025e84fb32b2d1e09e0128fa540637c41
dspopravko <demid1498@gmail.com>
2022-10-20 11:10:08 +0400

    shadow onScroll for mobile header

--- src/header/mobile/HeaderMobile.tsx
+++ src/header/mobile/HeaderMobile.tsx
@@ -1,13 +1,15 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import s from './HeaderMobile.module.css'
 import {Navbar} from "../../nav/Navbar";
 import {DarkModeBtn} from "../DarkModeBtn";
+import {ThemeContext} from "../../Context";
 
 export const HeaderMobile = () => {
     const [open, setOpen] = useState(false)
+    const {y} = useContext(ThemeContext);
     return (
         <>
-            <div className={open ? s.mobileMenu : s.mobileMenu + ' shadowUp'}>
+            <div className={open ? s.mobileMenu : y > 50 ? s.mobileMenu + ' shadowUp' : s.mobileMenu }>
                 <div className={s.title} onClick={()=> {window.scrollTo(0, 0)}}>
                     Demid
                 </div>
